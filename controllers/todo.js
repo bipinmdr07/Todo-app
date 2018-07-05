d7c7c06e9144eff53a9f8fc39d9d4e625ffdc3ac
Bipin Manandhar <kubipin07@gmail.com>
2018-07-06 01:18:25 +0545

    Setup all CRUD operation in the database

--- controllers/todo.js
+++ controllers/todo.js
@@ -3,7 +3,7 @@ const database  = require('../db');
 
 // handling get and post request
 router.route('/')
-  // 
+  // all todos
   .get((req, res) => {
     database('todos').select()
       .then((todos) => {
@@ -14,8 +14,64 @@ router.route('/')
       });
   })
 
+  // adding todo
   .post((req, res) => {
+    const todoData = req.body;
+    // validate the presence of task and due_date
+    for (let requiredParams of ['task', 'due_date']) {
+      if (!todoData[requiredParams]) {
+        return res
+          .status(422)
+          .send({ error: `You are missing "${requiredParams}" property` });
+      }
+    }
 
+    // if valid save it to database
+    database('todos').insert(todoData, 'id')
+      .then(todo => {
+        res.status(201).json({ id: todo[0] });
+      })
+      .catch(error => {
+        res.status(500).json({ error });
+      });
   });
+
+// handling get, put and delete for specific todo item
+router.route("/:id")
+  // get specific todo item
+  .get((req, res) => {
+    database('todos').where('id', req.params.id).select()
+      .then(todos => {
+        if (todos.length === 1) {
+          res.status(200).json(todos[0])
+        } else {
+          res.status(404).json({
+            error: `Could not find todo with id "${req.params.id}"`
+          });
+        }
+      });
+  })
+
+  // update specific todo item
+  .put((req, res) => {
+    database('todos').where('id', req.params.id).update(req.body, 'id')
+      .then((todos) => {
+        res.status(200).json(todos[0]);
+      })
+      .catch((error) => {
+        res.status(500).json({ error });
+      });
+  })
+  
+  // delete specific todo item
+  .delete((req, res) => {
+    database('todos').where('id', req.params.id).del()
+      .then(() => {
+        res.status(200).json({ "deleted": "successfully" });
+      })
+      .catch((error) => {
+        res.status(500).json({ error });
+      });
+  })
 
 module.exports = router;
--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const routes = require('./routes');
+const bodyParser = require('body-parser');
 
 const PORT = 3000;
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(express.json());
 app.use('/api', routes);
 
 app.listen(PORT, (req, res) => {
